b0cf984637d54a299e5849c9aeeb4e034e473208
doctormee <wandrsl@gmail.com>
2017-04-24 18:42:59 +0300

    User added

---
+++ include/model/User.h
@@ -0,0 +1,18 @@
+#include <string>
+class User {
+    int uid;
+    std::string name, surname;
+    int age, height, weight;
+    char gender;
+public:
+    User(int);
+    User(int, std::string, std::string, int, int, int, char);
+    std::string get_name() const;
+    std::string get_surname() const;
+    int get_age() const;
+    int get_height() const;
+    int get_weight() const;
+    char get_gender() const;
+    int get_uid() const;
+    void set_uid(int);
+};
---
+++ src/model/User.cpp
@@ -0,0 +1,45 @@
+#include <User.h>
+
+User::User(int uid_) {
+    uid = uid_;
+    name = "John";
+    surname = "Appleseed";
+    age = 21;
+    height = 180;
+    weight = 80;
+    gender = 'M';
+}
+
+User::User(int uid_, std::string name_, std::string surname_, int age_, int height_, int weight_, char gender_): uid(uid_), name(name_), surname(surname_), age(age_), height(height_), weight(weight_), gender(gender_) {};
+
+std::string User::get_name() const {
+    return name;
+}
+
+std::string User::get_surname() const {
+    return surname;
+}
+
+int User::get_age() const {
+    return age;
+}
+
+int User::get_height() const {
+    return height;
+}
+
+int User::get_weight() const {
+    return weight;
+}
+
+char User::get_gender() const {
+    return gender;
+}
+
+int User::get_uid() const {
+    return uid;
+}
+
+void User::set_uid(int uid_) {
+    uid = uid_;
+}
