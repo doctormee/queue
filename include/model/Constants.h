45b4aa914980805896b8eac64de50ed4807b3e0e
doctor_mee <wandrsl@gmail.com>
2017-04-26 16:02:49 +0300

    Yet another take on constants

--- include/model/Constants.h
+++ include/model/Constants.h
@@ -1,13 +1,9 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
-namespace Field {
-    enum Field: int {
+enum Field: int {
     ALL, AGE, HEIGHT, WEIGHT, GENDER
-    };
-}
-namespace Sign {
-    enum Sign: int {
+};
+enum Sign: int {
         LESS, LESSEQ, EQ, MOREEQ, MORE, NOTEQ
-    };
-}
+};
 #endif
