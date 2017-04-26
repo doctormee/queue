35e3e589c32e8541a9ccb9d9c7aa9fd90abdb3f8
doctor_mee <wandrsl@gmail.com>
2017-04-26 11:17:01 +0300

    Rule methods implemented

--- include/model/Rule.h
+++ include/model/Rule.h
@@ -8,8 +8,8 @@ class Rule {
     int value1, value2;
 public:
     Rule(Field, Sign, int, Field, Sign, int);
-    Field get_field(int);
-    Sign get_sign(int);
-    int get_value(int);
+    Field get_field(int) const;
+    Sign get_sign(int) const;
+    int get_value(int) const;
 };
 #endif
--- src/model/Rule.cpp
+++ src/model/Rule.cpp
@@ -11,3 +11,42 @@ public:
     int get_value(int);
 }; */
 Rule::Rule(Field field1_, Sign sign1_, int value1_, Field field2_, Sign sign2_, int value2_): field1(field1_), field2(field2_), sign1(sign1_), sign2(sign2_), value1(value1_), value2(value2_) {};
+Field Rule::get_field(int i) const{
+    switch (i) {
+        case 1: {
+            return field1;
+        }
+        case 2: {
+            return field2;
+        }
+        default: {
+            return 0;
+        }
+    }
+}
+Sign Rule::get_sign(int i) const{
+    switch (i) {
+        case 1: {
+            return sign1;
+        }
+        case 2: {
+            return sign2;
+        }
+        default: {
+            return 0;
+        }
+    }
+}
+int Rule::get_value(int i) const {
+    switch (i) {
+        case 1: {
+            return value1;
+        }
+        case 2: {
+            return value2;
+        }
+        default: {
+            return 0;
+        }
+    }
+}
