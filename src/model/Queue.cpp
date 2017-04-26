d13bece9e13de44b96641cb4aed8c6754ca2d856
doctor_mee <wandrsl@gmail.com>
2017-04-26 10:09:47 +0300

    Picked up from test

--- src/model/Queue.cpp
+++ src/model/Queue.cpp
@@ -14,14 +14,15 @@ public:
 #include "Queue.h"
 Queue::Item::Item(User *user_): user(user_), priority(0) {}
 void Queue::push(User* user_) {
-    container.push_back(new Item(user_));
+    std::unique_ptr<Item> tmp(new Item(user_));
+    container.push_back(std::move(tmp));
 }
 void Queue::update(std::vector<std::unique_ptr<Rule>> &rules) {
     for (auto &i: container) {
         i->priority = 0;
         for (auto &j: container) {
             for (auto &k:rules) {
-                i->priority += *k(i->user, j->user);
+                i->priority += (*k)(*(i->user), *(j->user));
             }
         }
     }
