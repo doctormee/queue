87fae3b2a7692e9fac2ba54d38a02c6c48e2ded2
doctor_mee <wandrsl@gmail.com>
2017-04-26 15:57:12 +0300

    developing new test

--- src/interface/test.cpp
+++ src/interface/test.cpp
@@ -4,9 +4,12 @@
 #include <memory>
 #include <Specialist.h>
 #include <Queue.h>
-
+#include "Rule.h"
+#include "Constants.h"
 
 int main() {
-    
+    Rule* r = new Rule(FIELD::AGE, SIGN::LESS, 10, FIELD::AGE, SIGN::MORE, 30);
+    std::cout << r->get_field(1);
+    delete r;
     return 0;
 }
