d21be1670a76b19d83c0c28faad9c4dc57208fe0
doctor_mee <wandrsl@gmail.com>
2017-05-12 01:32:22 +0300

    Improved tests

--- src/model/NaryPredicate.cpp
+++ src/model/NaryPredicate.cpp
@@ -1,20 +1,8 @@
-/*
-#pragma once
-#include <memory>
-#include "Predicate.h"
-class Visitor;
-class NaryPredicate: public Predicate {
-    std::shared_ptr<Predicate> left;
-    std::shared_ptr<Predicate> right;
-public:
-    std::shared_ptr<Predicate> get_left();
-    std::shared_ptr<Predicate> get_right();
-};
-*/
+
 #include "NaryPredicate.h"
 void NaryPredicate::add(std::shared_ptr<Predicate> f) { 
     if (!f) {
-        throw std::logic_error("Null pointer is added");
+        throw std::logic_error("Empty predicate (i.e. nullptr) is added");
     }
     operands.push_back(f);
 }
--- tests/predicate_test.cpp
+++ tests/predicate_test.cpp
@@ -42,7 +42,7 @@ TEST(Term_test, Test_Evaluation) {
         ASSERT_TRUE((i.accept(eval), eval.get_answer()));
     }
 } 
-TEST(Binary_Predicate_Test, Binary_constructor_and_eval) {
+TEST(Binary_Predicate_Test, Binary_constructor_add_and_eval) {
     std::shared_ptr<Predicate> t1(new Term(Field::HEIGHT, Sign::MORE, 190));
     std::shared_ptr<Predicate> t2(new Term(Field::GENDER, Sign::EQ, 'F'));
     User ivan(2, "Ivan", "Ivanov", 10, 192, 70, 'F');
@@ -71,14 +71,28 @@ TEST(Binary_Predicate_Test, OR_Binary) {
     std::shared_ptr<Term> t2(new Term(Field::GENDER, Sign::EQ, 'F'));
     std::shared_ptr<Term> t3(new Term(Field::WEIGHT, Sign::NOTEQ, 71));
     User ivan(2, "Ivan", "Ivanov", 10, 192, 70, 'F');
-    std::shared_ptr<ConjunctionPredicate> bin(new ConjunctionPredicate());
-    bin->add(t1);
-    bin->add(t2);
+    std::shared_ptr<ConjunctionPredicate> bin(new ConjunctionPredicate(t1, t2));
     std::shared_ptr<DisjunctionPredicate> bin2(new DisjunctionPredicate());
     bin2->add(bin);
     bin2->add(t3);
     Evaluator eval(&ivan);
     ASSERT_TRUE((bin2->accept(eval), eval.get_answer()));
+} 
+TEST(Binary_Predicate_Test, IF_Binary) {
+    std::shared_ptr<Term> t1(new Term(Field::HEIGHT, Sign::MORE, 190));
+    std::shared_ptr<Term> t2(new Term(Field::GENDER, Sign::EQ, 'M'));
+    User ivan(2, "Ivan", "Ivanov", 10, 192, 70, 'F');
+    std::shared_ptr<ImplicationPredicate> imp1(new ImplicationPredicate(t1, t2));
+    std::shared_ptr<ImplicationPredicate> imp2(new ImplicationPredicate(t2, t1));
+    std::shared_ptr<ConjunctionPredicate> con(new ConjunctionPredicate(imp1, imp2));
+    std::shared_ptr<DisjunctionPredicate> dis(new DisjunctionPredicate(imp1, imp2));
+    std::shared_ptr<ImplicationPredicate> imp11(new ImplicationPredicate(t1, t1));
+    std::shared_ptr<ImplicationPredicate> imp22(new ImplicationPredicate(t2, t2));
+    Evaluator eval(&ivan);
+    ASSERT_FALSE((con->accept(eval), eval.get_answer()));
+    ASSERT_TRUE((dis->accept(eval), eval.get_answer()));
+    ASSERT_TRUE((imp11->accept(eval), eval.get_answer()));
+    ASSERT_TRUE((imp22->accept(eval), eval.get_answer()));
 } 
 
 TEST(Unary_test, Unary_Construction_and_Eval) {
