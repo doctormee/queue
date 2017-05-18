8e080b729ae8849ffd54dd6425a7654d4feea282
doctor_mee <wandrsl@gmail.com>
2017-05-18 11:04:22 +0300

    Moved queue

--- src/model/Queue.cpp
+++ src/model/Queue.cpp
@@ -27,10 +27,12 @@ void Queue::sort() {
     std::sort(container.begin(), container.end(), Queue::queue_less);
 }
 
-std::vector<std::unique_ptr<Queue::Item>>::iterator Queue::begin() {
+std::vector<std::unique_ptr<Queue::Item>>::iterator
+Queue::begin() {
     return container.begin();
 }
-std::vector<std::unique_ptr<Queue::Item>>::iterator Queue::end() {
+std::vector<std::unique_ptr<Queue::Item>>::iterator
+Queue::end() {
     return container.end();
 }
 
