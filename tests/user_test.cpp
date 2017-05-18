10be0faedf86911fe1be40f0e569d6440b0d2e67
doctor_mee <wandrsl@gmail.com>
2017-05-18 11:16:07 +0300

    Fixing tests

--- tests/user_test.cpp
+++ tests/user_test.cpp
@@ -15,7 +15,7 @@ TEST(User_Test, Initializing_vector) {
     std::vector<std::unique_ptr<User>> vect;
     add(vect, (new User{1}));
     add(vect, (new User{2, "Ivan", "Ivanov", 10, 192, 70, 'F'}));
-    ASSERT_EQ(vect.size(), 2);
+    ASSERT_EQ(int(vect.size()), 2);
     ASSERT_EQ(vect[0]->get_uid(), 1);
     ASSERT_EQ(vect[1]->get_uid(), 2);
 }
